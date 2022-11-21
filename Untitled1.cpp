e57cd5b4b2c179cbe8e39fe4e408c8404ce4b340
Muhammad Nauman <86490074+naumanbutt2002@users.noreply.github.com>
2022-11-21 11:00:52 +0500

    LMS

---
+++ Untitled1.cpp
@@ -0,0 +1,16 @@
+	/* Lab 05 Basics of C++ [BSCS 2020 A] 
+		CS102-Programming Fundamentals Lab
+		Author : M.Nauman
+		Created at: 10.12.2020
+	*/
+	
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{	
+	cout << "HELLO" << ;
+	
+		
+	return(0);
+}
